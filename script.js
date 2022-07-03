6ff0558927f5f73e2a8a3a73394667f97dbafbea
Elmer Rocha <elmer.rocha.j@gmail.com>
2022-07-02 20:52:15 -0500

    IBM

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    const principal = document.getElementById('principal').value;
+    if (principal <= 0) {
+        alert('Enter a positive number');
+        document.getElementById('principal').focus();
+    }
+    else {
+        let rate = document.getElementById('rate').value;
+        let years = document.getElementById('years').value;
+        let interest = principal * years * rate /100;
+        let year = new Date().getFullYear()+parseInt(years);
+        document.getElementById("result").innerHTML="If you deposit "+principal+
+        ",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+
+        interest+",\<br\>in the year "+year+"\<br\>"
+    }
 }
-        
+function updateRate() {
+    let rateval = document.getElementById('rate').value;
+    document.getElementById('rate_val').innerText=rateval;
+}
