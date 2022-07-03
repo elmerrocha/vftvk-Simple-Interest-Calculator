4634f9c18651e4cb49308e658ddf425316878f7b
Elmer Rocha <elmer.rocha.j@gmail.com>
2022-07-02 21:29:01 -0500

    Yellow color

--- script.js
+++ script.js
@@ -9,9 +9,11 @@ function compute() {
         let years = document.getElementById('years').value;
         let interest = principal * years * rate /100;
         let year = new Date().getFullYear()+parseInt(years);
-        document.getElementById("result").innerHTML="If you deposit "+principal+
-        ",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+
-        interest+",\<br\>in the year "+year+"\<br\>"
+        document.getElementById("result").innerHTML=`If you deposit \<span style="background-color:yellow;">`+principal+
+        `\</span>,\<br\>at an interest rate of \<span style="background-color:yellow;">`+rate+
+        `\</span>%\<br\>You will receive an amount of \<span style="background-color:yellow;">`+interest+
+        `\</span>,\<br\>in the year \<span style="background-color:yellow;">`+year+"\</span>\<br\>"
+        }
     }
 }
 function updateRate() {
